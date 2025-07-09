a270ee3496e7a65b20034220e889dd583a88342d
SonPham2004 <114486900+Olin2004@users.noreply.github.com>
2025-07-09 15:38:22 +0700

    Update apiConfig.js

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const API_BASE_URL = "http://localhost:8080/api";
+export const API_BASE_URL = "https://4f2a3a08eafd.ngrok-free.app/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
